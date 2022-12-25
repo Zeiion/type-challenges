40f61fce162733fec02a0c6dbf4b6df1f7805bcf
Zeiion <Zeiion@qq.com>
2022-12-25 19:42:26 +0800

    easy complete

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,12 @@
-type Includes<T extends readonly any[], U> = any
+type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y
+  ? 1
+  : 2
+  ? true
+  : false
+// 很奇怪，复制一遍Equal才对
+
+type Includes<T extends readonly any[], U> = T extends [infer K, ...infer R]
+  ? Equal<U, K> extends true
+    ? true
+    : Includes<R, U>
+  : false
