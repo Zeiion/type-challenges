f5c60a9b25a8c9aa8dc168e4193c8ac13994d172
Zeiion <Zeiion@qq.com>
2023-03-28 14:43:15 +0800

    2 medium

--- questions/00612-medium-kebabcase/template.ts
+++ questions/00612-medium-kebabcase/template.ts
@@ -1,1 +1,7 @@
-type KebabCase<S> = any
+type KebabCase<S> = S extends `${infer First}${infer Rest}`
+  ? Rest extends Uncapitalize<Rest>
+    ? `${Lowercase<First>}${KebabCase<Rest>}`
+    : `${Lowercase<First>}-${KebabCase<Rest>}`
+  : S
+
+const testset: KebabCase<'😎'> = '😎'
--- questions/00645-medium-diff/template.ts
+++ questions/00645-medium-diff/template.ts
@@ -1,1 +1,4 @@
-type Diff<O, O1> = any
+type Diff<O extends object, O1 extends object> = {
+  [key in Exclude<keyof O, keyof O1> | Exclude<keyof O1, keyof O>]: (O &
+    O1)[key]
+}
