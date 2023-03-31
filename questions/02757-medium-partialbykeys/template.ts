90f949e279732489f1de347371d9a9868f143322
Zeiion <Zeiion@qq.com>
2023-03-31 17:55:22 +0800

    4 medium

--- questions/02757-medium-partialbykeys/template.ts
+++ questions/02757-medium-partialbykeys/template.ts
@@ -1,1 +1,8 @@
-type PartialByKeys<T, K> = any
+type PartialByKeys<
+  T extends Record<PropertyKey, any>,
+  K extends keyof T = keyof T,
+> = Copy<
+  Omit<T, K> & {
+    [key in K]?: T[key]
+  }
+>
