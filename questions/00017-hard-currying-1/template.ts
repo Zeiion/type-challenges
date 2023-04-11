223794bd85dd4dd6b26a1c9ccbddc05f461a9721
Zeiion <Zeiion@qq.com>
2023-04-11 19:25:47 +0800

    2 hard

--- questions/00017-hard-currying-1/template.ts
+++ questions/00017-hard-currying-1/template.ts
@@ -1,1 +1,8 @@
-declare function Currying(fn: any): any
+type Curried<F> = F extends (...args: infer A) => infer R
+  ? A extends [infer First, ...infer Other]
+    ? (arg: First) => Curried<(...args: Other) => R>
+    : R
+  : never
+declare function Currying<F>(
+  fn: F,
+): Curried<F> extends Function ? Curried<F> : () => Curried<F> // TODO
