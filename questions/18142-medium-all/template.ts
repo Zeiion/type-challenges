024e860e5fc2a6a5810215a515d6c0a42b2bb8c2
Zeiion <Zeiion@qq.com>
2023-04-26 14:51:20 +0800

    2 medium

--- questions/18142-medium-all/template.ts
+++ questions/18142-medium-all/template.ts
@@ -1,1 +1,5 @@
-type All = any
+type All<T extends any[], ele> = T extends [infer F, ...infer R]
+  ? Equal<F, ele> extends true
+    ? All<R, ele>
+    : false
+  : true
--- questions/18220-medium-filter/template.ts
+++ questions/18220-medium-filter/template.ts
@@ -1,1 +1,14 @@
-type Filter<T extends any[], P> = []
+type Filter<T extends any[], P> = T extends [infer F, ...infer R]
+  ? F extends P
+    ? [F, ...Filter<R, P>]
+    : Filter<R, P>
+  : []
+
+// type Filter<T extends any[], P, ans extends any[] = []> = T extends [
+//   infer F,
+//   ...infer R,
+// ]
+//   ? F extends P
+//     ? Filter<R, P, [...ans, F]>
+//     : Filter<R, P, ans>
+//   : ans
